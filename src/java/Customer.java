736e417a20a932740d189c9ca1791b083cc93fd4
Matthew Wood <mw621@msstate.edu>
2013-11-04 19:53:35 -0600

    (Incomplete) Added Customer and customerCreateAccount Java Classes
    Added these classes.  No errors but still a work in progress.

---
+++ src/java/Customer.java
@@ -0,0 +1,116 @@
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/*
+ * Represents a Customer of Lauren's List.
+ */
+public class Customer {
+    private String userType;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    
+    public Customer()
+    {
+        userType = "customer";
+        email = "";
+        password = "";
+        firstName = "";
+        lastName = "";
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="Getters and Setters.">
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    // </editor-fold>
+    
+    
+    public boolean login(String email, String password, PrintWriter out, HttpServletRequest request)
+    {
+        DBConnections dataSource = DBConnections.getInstance();
+        Connection conn = dataSource.getConnection();
+        Statement statement = null;
+        ResultSet customerResult;
+        
+        boolean customerLogin = false;
+        try 
+        {
+            statement = conn.createStatement();
+            customerResult = statement.executeQuery("SELECT * FROM Customer WHERE email=\'" + email + "\' AND password=MD5(\'" + password + "\')");
+            customerLogin = customerResult.next();
+            if(customerLogin)
+            {
+               
+                HttpSession httpSession = request.getSession();
+                                
+                setEmail(customerResult.getString("email"));
+                setFirstName(customerResult.getString("first_name"));
+                setLastName(customerResult.getString("last_name"));
+                                
+                httpSession.setAttribute("user", this);
+                //httpSession.setAttribute("userType", userType);
+            }
+        } 
+        catch (SQLException ex)
+        {
+            out.println("SQLException in Query.java");
+            ex.printStackTrace(out);
+        }
+        finally
+        {
+            DBUtilities.closeStatement(statement);
+            dataSource.freeConnection(conn);
+        }
+        
+        return customerLogin;
+    }
+        
+    public void logout(HttpServletRequest request)
+    {
+        HttpSession httpSession = request.getSession();
+        
+        setPassword("");
+        setEmail("");
+        setFirstName("");
+        setLastName("");
+        
+        httpSession.invalidate();
+    }
+    
+}
---
+++ src/java/customerCreateAccount.java
@@ -0,0 +1,102 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*
+ * Signs up a new Customer.
+ */
+public class customerCreateAccount extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+    {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        
+        DBConnections dataSource = DBConnections.getInstance();
+        Connection conn = dataSource.getConnection();
+        PreparedStatement stat = null;
+        Statement statement = null;
+        ResultSet customerResult = null;
+        ResultSet managerResult = null;
+        
+        try {
+            String firstName = request.getParameter("firstName");
+            String lastName = request.getParameter("lastName");
+            String email = request.getParameter("email");
+            String password = request.getParameter("password");
+            String confirmPassword = request.getParameter("confirmPassword");
+
+            if (email.equals("") || password.equals("") || confirmPassword.equals("")) {
+                response.sendRedirect("createAccount.html");
+            } else if (!password.equals(confirmPassword)) {
+                response.sendRedirect("createAccount.html");
+            } else {
+
+                try {
+                    statement = conn.createStatement();
+                    customerResult = statement.executeQuery("SELECT * FROM Customers WHERE email=\'" + email + "\'");
+                    boolean customerFound = customerResult.next();
+                    //managerResult = statement.executeQuery("SELECT * FROM Managers WHERE ID=\'" + email + "\'");
+                    //boolean managerFound = managerResult.next();
+
+                    if (!customerFound)/* && !managerFound)*/ {
+                        String statString = "INSERT INTO Customers (`email`, `password`, `first_name`"
+                                + ", `last_name`) VALUES (?, MD5(?), ?, ?)";
+                        stat = conn.prepareStatement(statString);
+                        stat.setString(1, email);
+                        stat.setString(2, password);
+                        stat.setString(3, firstName);
+                        stat.setString(4, lastName);
+                        stat.executeUpdate();
+                        response.sendRedirect("homePage");
+                    } else {
+                        response.sendRedirect("createAccount.html");
+                    }
+                } catch (SQLException ex) {
+                    out.println("SQLException in Query.java");
+                    ex.printStackTrace(out);
+                } finally
+                {
+                    DBUtilities.closeResultSet(managerResult);
+                    DBUtilities.closeResultSet(customerResult); 
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace(out);
+        } finally {
+            out.close();
+            DBUtilities.closeStatement(stat);
+            DBUtilities.closeStatement(statement);
+            dataSource.freeConnection(conn);
+        }
+    }
+
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
